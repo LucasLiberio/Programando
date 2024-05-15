c15a9bbb010c599f8c4aa85bc6770966c6f3259f
Lucas Libério <102379989+LucasLiberio@users.noreply.github.com>
2024-05-15 09:32:30 -0300

    python

---
+++ links/Python/Python.py
@@ -0,0 +1,26 @@
+print ("print ('Olá, Mundo!') = " + ('Olá, Mundo!'))
+print ("print (7+4) = " , 7+4 )
+print ("print ('7' + '4') = " , '7' + '4')
+print ("print ('Olá' , 5) = " , 'Olá' , 5)
+
+nome = 'Guanabara'
+idade = 25
+peso = 75.8
+print (nome, idade, peso)
+
+nome = input ('Qual é seu nome? ')
+idade = input ('Qual é sua idade? ')
+peso = input ('Qual é seu peso? ')
+print (nome, idade, peso)
+
+nome = input ('Qual é seu nome? ')
+print ('Bem vindo sr', nome,'!!')
+
+dia = input ('Que dia voce nasceu? ')
+mes = input ('Qual mes vc nasceu? ')
+ano = input ('Qual ano vc nasceu? ')
+print ('A data do seu nascimento é',dia,'/',mes,'/',ano,'!')
+
+n1 = input ('Digite um numero ')
+n2 = input ('Digete mais um numero ')
+print ('A soma desses dois numero é =', int(n1)+int(n2))
---
+++ links/Python/teste01.py
@@ -0,0 +1,3 @@
+n1 = input ('Digite um numero ')
+n2 = input ('Digete mais um numero ')
+print ('A some desses dois numero é =', int(n1)+int(n2))
